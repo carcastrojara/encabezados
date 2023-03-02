74deb2ce4b3e49e50caa5b6b17cbc92853206309
Carlos Castro <carcastrojara@gmail.com>
2023-03-02 14:07:37 -0300

    add app.js

--- app.js
+++ app.js
@@ -4,7 +4,12 @@ const app = express();
 app.get('/', (req, res) => {
     console.log(req.headers['sec-ch-ua'])
   //res.send(`<h1>El browser es: ${req.headers['user-agent']}<h1/>`);
-  res.redirect(`/${req.headers['sec-ch-ua']}`);
+  //res.redirect(`/${req.headers['sec-ch-ua']}`);
+  res.redirect('/chrome')
+});
+app.get('/chrome', (req, res) => {
+//res.send(`<h1>El browser es: ${req.headers['user-agent']}<h1/>`);
+res.send(`<h1>El browser es: ${req.headers['user-agent']}<h1/>`);
 });
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
